daa089f72e03ba10226ea52d816b6e7b744a67e8
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-02 03:27:01 +0530

    Create Thread_Natural_No_Sum.c

---
+++ Assignment 8/Thread_Natural_No_Sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<pthread.h>
+void *sum (void *arg) {
+	int n;
+	printf("Enter the number: ");
+	scanf("%d", &n);
+	int sum=0;
+	for(int i=0;i<=n;i++){
+		sum+=i;
+	}
+	printf("Total Sum: %d\n",sum);
+	pthread_exit(NULL);
+}
+int main(){
+	pthread_t th1;
+	pthread_create(&th1,NULL,sum,NULL);
+	pthread_join(th1,NULL);
+	printf("exit");
+	return 0;
+}
