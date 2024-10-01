a953e376ca9f196ea05229eaaa823a420ba1022b
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-01 23:24:51 +0530

    Create Leap_Year.sh

---
+++ Assignment 1/Leap_Year.sh
@@ -0,0 +1,11 @@
+read -p "Enter a year:" year
+if [ $((year % 400)) -eq 0 ]; then
+	echo "$year is a leap year:"
+elif [ $((year % 100)) -eq 0 ]; then
+	echo "$year is not a leap year"
+elif [ $((year % 4)) -eq 0 ]; then 
+	echo "$year is a leap year"
+else
+	echo "$year is not a leap year:"
+fi
+
