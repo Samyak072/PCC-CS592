4fa70ec433fa818689b3bdeefea6232b81a903b9
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-02 03:19:50 +0530

    Update Signal.c

--- Assignment 7/Signal.c
+++ Assignment 7/Signal.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+void handle_signal(int signal_num) {
+    printf("\nReceived signal %d, terminating the process.\n", signal_num);
+    signal(SIGINT, SIG_DFL);
+    raise(SIGINT); 
+}
+int main() {
+    signal(SIGINT, handle_signal);
+    while (1) {
+        printf("Running... Press Ctrl+C to stop.\n");
+        sleep(1);  // Sleep for 1 second to avoid spamming the console
+    }
+    return 0;
+}
 
